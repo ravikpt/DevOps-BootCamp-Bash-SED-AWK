96b4cd6b5075759b9231f56782c1bee3caf1484c
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-26 23:28:12 +0400

    Adding my script

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-# Put you code here
+# copy passwd file!
+cp passwd passwd_new
+
+awk 'BEGIN{OFS=FS=":"}$1=="saned"{$7="/bin/bash"}{print}' passwd_new > tmp && mv tmp passwd_new
+sed -i '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
+awk 'BEGIN{FS=":"; OFS=":"} {print $1,$3,$5,$7}' passwd_new > tmp && mv tmp passwd_new
+sed -i '/daemon/d' passwd_new
+awk 'BEGIN{OFS=FS=":"}$2%2==0{$4="/bin/zsh"}{print}' passwd_new > tmp && mv tmp passwd_new
